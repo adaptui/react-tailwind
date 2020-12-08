7adb4f4c75244699cbaa8b2a44363dcc54151681
Navin <navin007.a@gmail.com>
2020-12-08 19:30:27 +0530

    feat(tailwind): ✨  use plugin addComponents for styling

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -35,15 +35,11 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   ...props
 }) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  const mode = primary ? "button--primary" : "button--secondary";
 
   return (
     <AriaButton
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " ",
-      )}
+      className={["button", `button--${size}`, mode].join(" ")}
       style={{ backgroundColor }}
       {...props}
     >
