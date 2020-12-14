e6a784d099f93a555d6040d0c61b64bc48808404
Navin Moorthy <navin007.a@gmail.com>
2020-12-14 19:27:41 +0530

    feat(calendar): ✨  add new api calendar (#6)

--- src/calendar/stories/Calendar.stories.tsx
+++ src/calendar/stories/Calendar.stories.tsx
@@ -2,18 +2,101 @@ import React from "react";
 import { Story, Meta } from "@storybook/react";
 
 import "./calendar.css";
-import { useCalendarState } from "@renderlesskit/react";
-import { Calendar } from "../Calendar";
+import {
+  ChevronLeft,
+  ChevronRight,
+  DoubleChevronLeft,
+  DoubleChevronRight,
+} from "../Icons";
+import {
+  Calendar,
+  CalendarHeader,
+  CalendarNextMonthButton,
+  CalendarNextYearButton,
+  CalendarPreviousMonthButton,
+  CalendarPreviousYearButton,
+  CalendarTable,
+  CalendarTableBody,
+  CalendarTableBodyData,
+  CalendarTableBodyDataButton,
+  CalendarTableBodyRow,
+  CalendarTableHead,
+  CalendarTableHeadHeader,
+  CalendarTableHeadHeaderAbbr,
+  CalendarTableHeadRow,
+  CalendarTitle,
+} from "../Calendar";
+import { useCalendarContext } from "../helpers";
 
 export default {
   title: "Calendar",
   component: Calendar,
-  argTypes: {},
 } as Meta;
 
 const Template: Story = () => {
-  const state = useCalendarState();
-  return <Calendar {...state} />;
+  return (
+    <Calendar>
+      <CalendarHeader>
+        <CalendarPreviousYearButton>
+          <DoubleChevronLeft />
+        </CalendarPreviousYearButton>
+        <CalendarPreviousMonthButton>
+          <ChevronLeft />
+        </CalendarPreviousMonthButton>
+        <CalendarTitle />
+        <CalendarNextMonthButton>
+          <ChevronRight />
+        </CalendarNextMonthButton>
+        <CalendarNextYearButton>
+          <DoubleChevronRight />
+        </CalendarNextYearButton>
+      </CalendarHeader>
+      <CalendarTable>
+        <CalendarTableHead>
+          <CalendarTableHeadRow>
+            <CalendarTableHeadColumns />
+          </CalendarTableHeadRow>
+        </CalendarTableHead>
+        <CalendarTableBody>
+          <CalendarTableBodyContents />
+        </CalendarTableBody>
+      </CalendarTable>
+    </Calendar>
+  );
+};
+
+const CalendarTableHeadColumns = (props: any) => {
+  const state = useCalendarContext();
+
+  return (
+    <>
+      {state.weekDays.map((day, dayIndex) => {
+        return (
+          <CalendarTableHeadHeader key={dayIndex} dayIndex={dayIndex}>
+            <CalendarTableHeadHeaderAbbr day={day} />
+          </CalendarTableHeadHeader>
+        );
+      })}
+    </>
+  );
+};
+
+const CalendarTableBodyContents = (props: any) => {
+  const state = useCalendarContext();
+
+  return (
+    <>
+      {state.daysInMonth.map((week, weekIndex) => (
+        <CalendarTableBodyRow key={weekIndex}>
+          {week.map((day, dayIndex) => (
+            <CalendarTableBodyData day={day} key={dayIndex} dayIndex={dayIndex}>
+              <CalendarTableBodyDataButton day={day} />
+            </CalendarTableBodyData>
+          ))}
+        </CalendarTableBodyRow>
+      ))}
+    </>
+  );
 };
 
 export const Default = Template.bind({});
