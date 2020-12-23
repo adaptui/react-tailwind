a2524218fb3e1b6476bfd23d1ae81f94a26fe46f
Anurag <hazru.anurag@gmail.com>
2020-12-23 12:24:10 +0530

    chore: migrate box

--- src/box/Box.tsx
+++ src/box/Box.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { Role, RoleProps } from "reakit";
 
-import { forwardRefWithAs } from "../utils/types";
+import { forwardRefWithAsSimple } from "../utils/types";
 
 export type BoxProps = RoleProps & {};
 
-export const Box = forwardRefWithAs<BoxProps, HTMLDivElement, "div">(
+export const Box = forwardRefWithAsSimple<BoxProps, HTMLDivElement, "div">(
   (props, ref) => {
     return <Role ref={ref} {...props} />;
   },
