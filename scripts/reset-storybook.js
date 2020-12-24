c4a970aa65e0ab8560b6d6f538c33ad0d17e4672
Anurag <hazru.anurag@gmail.com>
2020-12-24 23:21:35 +0530

    chore: test

--- scripts/reset-storybook.js
+++ scripts/reset-storybook.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const path = require("path");
+const exec = require("child_process").exec;
 
 const template = `module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
 `;
+
+exec("git add .storybook\\main.ts");
 
 fs.writeFileSync(path.join(process.cwd(), ".storybook", "main.ts"), template, {
   encoding: "utf-8",
