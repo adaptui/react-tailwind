fb3cd190af310400acc1d05989151481e897a181
Anurag <hazru.anurag@gmail.com>
2020-12-24 13:08:15 +0530

    refactor: refactored macro & added proper error messages

--- tailwindclass.macro.js
+++ tailwindclass.macro.js
@@ -1,19 +1,33 @@
-const { createMacro } = require("babel-plugin-macros");
-
-const { overrideTailwindClasses } = require("tailwind-override");
-const { cx } = require("@renderlesskit/react");
-const ocx = (...classNames) => overrideTailwindClasses(cx(...classNames));
-
-const theme = require("./tailwind.config");
 const { get } = require("lodash");
+const { cx } = require("@renderlesskit/react");
+const { overrideTailwindClasses } = require("tailwind-override");
+const { createMacro, MacroError } = require("babel-plugin-macros");
+const theme = require("./tailwind.config");
+
+const ocx = (...classNames) => overrideTailwindClasses(cx(...classNames));
 
 module.exports = createMacro(function ({ references, state, babel }) {
   references.default.forEach(referencePath => {
-    const [firstArg] = referencePath.parentPath.get("arguments");
+    const [componentPath] = referencePath.parentPath.get("arguments");
 
-    const argValue = firstArg.node.value;
+    if (!componentPath) {
+      throw new MacroError("Expected 1 argument but got 0");
+    }
+
+    const argValue = componentPath.node.value;
+    if (typeof argValue !== "string") {
+      throw new MacroError(
+        `Argument expected of type string but got ${typeof argValue}`,
+      );
+    }
+
     const themeValue = get(theme, argValue);
-    const functionCallPath = firstArg.parentPath;
+    if (typeof themeValue === "undefined") {
+      throw new MacroError(
+        `Unable to find component classes at path "${argValue}"`,
+      );
+    }
+    const functionCallPath = componentPath.parentPath;
 
     if (typeof themeValue === "object") {
       const values = Object.keys(themeValue).map(key => {
@@ -39,7 +53,7 @@ function tailwindOverride({ references, state, babel }) {
   references.default.forEach(referencePath => {
     const args = referencePath.parentPath.get("arguments");
 
-    let stringLiterals = args.map(v => {
+    const stringLiterals = args.map(v => {
       return v.node.value;
     });
     let classname = ocx(...stringLiterals);
