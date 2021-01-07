3ea24563ccc873a027f1c707d97455dbcc966495
Anurag Hazra <hazru.anurag@gmail.com>
2021-01-07 15:53:49 +0530

    test(tag): added tests for Tag (#30)
    * test(tag): added test for tag
    
    * test(tag): added TagGroup test & removed snapshots because of id change

--- src/Tag/Tag.tsx
+++ src/Tag/Tag.tsx
@@ -94,6 +94,8 @@ const ClosableElement: React.FC<{
 
   return (
     <CompositeItem
+      aria-label="close"
+      data-testid="testid-close-element"
       as={Clickable}
       className={theme.tag.suffix}
       onClick={handleClick}
