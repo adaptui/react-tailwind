07da6904315fed597001b4703133aa57d7324c1d
Anurag <hazru.anurag@gmail.com>
2021-01-12 18:31:52 +0530

    test: removed snapshot tests

--- src/icon/__tests__/Icon.test.tsx
+++ src/icon/__tests__/Icon.test.tsx
@@ -15,26 +15,48 @@ describe("<Icon />", () => {
   });
 
   it("should render custom svg icon", () => {
+    const viewbox = "0 0 100 100";
     const Custom = () => (
-      <svg viewBox={"0 0 100 100"}>
+      <svg data-testid="testid-customicon" viewBox={viewbox}>
         <path d="M12"></path>
       </svg>
     );
-    const { container } = render(<Icon as={Custom} />);
+    render(<Icon as={Custom} />);
 
     expect(screen.queryByTestId("testid-icon")).not.toBeInTheDocument();
-    expect(container).toMatchSnapshot();
+    expect(screen.queryByTestId("testid-customicon")).toBeInTheDocument();
+    expect(screen.queryByTestId("testid-customicon")).toHaveAttribute(
+      "viewBox",
+      viewbox,
+    );
   });
 
   it("should render as expected with component as div", () => {
-    const { container } = render(
+    render(
       <Icon as={"div"}>
-        <FaCross viewBox={"0 0 100 100"} />
+        <FaCross data-testid="testid-faicon" viewBox={"0 0 100 100"} />
       </Icon>,
     );
 
     expect(screen.queryByTestId("testid-icon")).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
+    expect(screen.queryByTestId("testid-icon")?.tagName).toBe("DIV");
+    expect(screen.queryByTestId("testid-faicon")).toBeInTheDocument();
+  });
+
+  it("should render custom icon with path children", () => {
+    render(
+      <Icon viewBox="0 0 200 200">
+        <path
+          data-testid="testid-path"
+          fill="currentColor"
+          d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+        />
+      </Icon>,
+    );
+
+    expect(screen.queryByTestId("testid-icon")).toBeInTheDocument();
+    expect(screen.queryByTestId("testid-icon")?.tagName).toBe("svg");
+    expect(screen.queryByTestId("testid-path")).toBeInTheDocument();
   });
 
   it("should not have a11y violations", async () => {
