1dd01dd3abd69b40a3c17fd368e8e2e6fa69680c
anuraghazra <hazru.anurag@gmail.com>
2021-01-19 12:15:58 +0530

    chore: remove css file

--- src/checkbox/stories/Checkbox.stories.tsx
+++ src/checkbox/stories/Checkbox.stories.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 
-import "./checkbox.css";
 import { Checkbox, CheckboxProps } from "../index";
 import { CheckboxInput } from "../Checkbox";
 import { CheckIcon } from "../../icons";
