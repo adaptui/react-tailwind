0dc4acb84b978ef62fec6f829481a6c24311f85e
Navin Moorthy <navin007.a@gmail.com>
2021-02-04 14:36:02 +0530

    refactor(checkbox): ♻️  improve checkbox api (#51)
    * refactor(checkbox): ♻️  improve checkbox logic
    
    * refactor(checkbox): 🏷️  update setState types
    
    * refactor(checkbox): ♻️  improve logic using context
    
    * refactor(checkbox): ♻️  move checkbox parts into its own file
    
    * refactor(checkbox): ♻️  update checkbox to new api
    
    * refactor(checkbox): ♻️  add forward ref

---
+++ src/checkbox/CheckboxText.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { cx } from "@renderlesskit/react";
+
+import { useTheme } from "../theme";
+import { Box, BoxProps } from "../box";
+import { useCheckboxContext } from "./Checkbox";
+import { forwardRefWithAs } from "../utils/types";
+
+export type CheckboxTextProps = BoxProps & {};
+
+export const CheckboxText = forwardRefWithAs<
+  CheckboxTextProps,
+  HTMLDivElement,
+  "div"
+>((props, ref) => {
+  const { className, ...rest } = props;
+
+  const { size = "sm" } = useCheckboxContext();
+
+  const theme = useTheme();
+  const checkboxLabelStyles = cx(
+    theme.checkbox.label.base,
+    theme.checkbox.label.size[size],
+    className,
+  );
+
+  return <Box className={checkboxLabelStyles} ref={ref} {...rest} />;
+});
