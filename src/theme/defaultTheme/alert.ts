f3b70fb051114dec4c1c969e965d1be93a049e03
Anurag <hazru.anurag@gmail.com>
2021-02-10 16:06:50 +0530

    chore: prefix lib

--- src/theme/defaultTheme/alert.ts
+++ src/theme/defaultTheme/alert.ts
@@ -2,15 +2,16 @@ export const alert = {
   base:
     "lib:font-sans lib:flex lib:items-center lib:w-full lib:min-h-10 lib:overflow-hidden lib:p-2 lib:px-3 lib:rounded-md",
   title:
-    "lib:flex lib:items-center lib:text-gray-800 lib:text-sm lib:leading-4 lib:font-semibold lib:mr-2",
+    "lib:flex lib:items-centerlib:text-gray-800 lib:text-sm lib:leading-4 lib:font-semibold lib:mr-2",
   description: "lib:inline lib:text-gray-600 lib:text-sm lib:leading-4",
   actionsWrapper: "items-center inherit ml-auto",
   icon: {
-    base: "flex-shrink-0 mr-3 w-3.5 h-3.5 max-w-3.5 max-h-3.5",
-    icons: "relative -top-0.5 lib:w-full lib:h-full",
+    base: "lib:flex-shrink-0 lib:mr-3 lib:w-4 lib:h-4 lib:max-w-4 lib:max-h-4",
+    icons: "lib:relative lib:-top-0.5 lib:w-full lib:h-full",
   },
   iconButton: {
-    base: "text-gray-800 h-5 px-0 bg-transparent min-w-5 ml-2 lib:shadow-none",
+    base:
+      "lib:text-gray-800 lib:h-5 lib:px-0 bg-transparent lib:min-w-5 lib:ml-2 lib:shadow-none",
   },
   actionButton: "lib:h-5 bg-transparent lib:px-0 lib:shadow-none",
   status: {
