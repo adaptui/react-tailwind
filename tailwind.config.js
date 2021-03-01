cafe7eef4b9e9e00a0bca137bf18aaf2c0a4513d
Anurag Hazra <hazru.anurag@gmail.com>
2021-03-01 12:53:55 +0530

    fix: separate theme from tailwind config (#87)
    * fix: separate theme from tailwind config
    
    * fix: ts theme system
    
    * chore: fix test theme config path
    
    * test: added tests for mergeTheme utilities
    
    * fix: 🐛  theme object rendering merged classes
    
    * chore: husky fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,4 @@ module.exports = preset({
   theme: {
     extend: {},
   },
-  // This only affected the Storybook, doesn't go or merge when used this config as preset
-  components: {
-    extend: {
-      button: {
-        variant: {
-          tertiary: "bg-purple-600 lib:text-white",
-        },
-        size: {
-          xxl:
-            "h-16 min-w-16 text-xl px-8 w-auto font-medium rounded-lg shadow-sm",
-        },
-      },
-      // alert: {
-      //   status: {
-      //     info: {
-      //       base: "bg-red-500",
-      //       icon: "text-white",
-      //     },
-      //   },
-      // },
-    },
-  },
 });
