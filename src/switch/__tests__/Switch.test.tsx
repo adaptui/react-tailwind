0b133ab87283350dbcd4d4514be6b071fe0c42ba
Anurag <hazru.anurag@gmail.com>
2021-03-01 15:48:33 +0530

    test: added test case for checked prop control

--- src/switch/__tests__/Switch.test.tsx
+++ src/switch/__tests__/Switch.test.tsx
@@ -33,6 +33,19 @@ describe("<Switch />", () => {
     expect(screen.getByRole("switch")).toBeChecked();
   });
 
+  it("should support controlled state with checked prop", () => {
+    const Controlled = () => {
+      const [state, setState] = React.useState(false);
+
+      return <Switch checked={state} onChange={e => setState(!state)} />;
+    };
+    render(<Controlled />);
+
+    expect(screen.getByRole("switch")).not.toBeChecked();
+    fireEvent.click(screen.getByRole("switch"));
+    expect(screen.getByRole("switch")).toBeChecked();
+  });
+
   it("should render with custom render prop", () => {
     const DARK_MODE = "Dark Mode";
     const LIGHT_MODE = "Light Mode";
