958c23fd5f75cb883b41cdace40d1388984e885d
Navin Moorthy <navin007.a@gmail.com>
2021-03-04 18:18:02 +0530

    refactor(toast): ♻️  fix the action button styles

--- src/theme/defaultTheme/toast.ts
+++ src/theme/defaultTheme/toast.ts
@@ -16,52 +16,52 @@ export const toast = {
   actions: {
     base: "space-x-2 lib:ml-auto lib:flex lib:items-center",
     button: {
-      ghost: "lib:text-white lib:hover:text-gray-800 lib:hover:bg-white",
-      primary: "lib:text-gray-800 lib:bg-white",
-      secondary: "lib:text-white lib:bg-gray-700",
+      ghost: "",
+      primary: "",
+      secondary: "",
     },
   },
   success: {
-    base: "lib:bg-gray-800 lib:text-white",
-    body: { description: "lib:text-gray-300" },
+    base: "bg-gray-800 text-white",
+    body: { description: "text-gray-300" },
     actions: {
       button: {
-        ghost: "lib:text-white lib:hover:text-gray-800 lib:hover:bg-white",
-        primary: "lib:text-gray-800 lib:bg-white",
-        secondary: "lib:text-white lib:bg-gray-700",
+        ghost: "text-white hover:text-gray-800 hover:bg-white",
+        primary: "text-gray-800 bg-white",
+        secondary: "text-white bg-gray-700",
       },
     },
   },
   info: {
-    base: "lib:bg-blue-600 lib:text-white",
-    body: { description: "lib:text-blue-300" },
+    base: "bg-blue-600 text-white",
+    body: { description: "text-blue-300" },
     actions: {
       button: {
-        ghost: "lib:text-white lib:hover:text-blue-800 lib:hover:bg-white",
-        primary: "lib:text-blue-800 lib:bg-white",
-        secondary: "lib:text-white lib:bg-blue-700",
+        ghost: "text-white hover:text-blue-800 hover:bg-white",
+        primary: "text-blue-800 bg-white",
+        secondary: "text-white bg-blue-700",
       },
     },
   },
   warning: {
-    base: "lib:bg-orange-500 lib:text-white",
-    body: { description: "lib:text-orange-300" },
+    base: "bg-orange-500 text-white",
+    body: { description: "text-orange-300" },
     actions: {
       button: {
-        ghost: "lib:text-white lib:hover:text-orange-80lib:0 hover:bg-white",
-        primary: "lib:text-orange-800 lib:bg-white",
-        secondary: "lib:text-white lib:bg-orange-700",
+        ghost: "text-white hover:text-orange-800 hover:bg-white",
+        primary: "text-orange-800 bg-white",
+        secondary: "text-white bg-orange-700",
       },
     },
   },
   error: {
-    base: "lib:bg-red-600 lib:text-white",
-    body: { description: "lib:text-red-300" },
+    base: "bg-red-600 text-white",
+    body: { description: "text-red-300" },
     actions: {
       button: {
-        ghost: "lib:text-white lib:hover:text-red-800 hlib:over:bg-white",
-        primary: "lib:text-red-800 lib:bg-white",
-        secondary: "lib:text-white lib:bg-red-700",
+        ghost: "text-white hover:text-red-800 hover:bg-white",
+        primary: "text-red-800 bg-white",
+        secondary: "text-white bg-red-700",
       },
     },
   },
