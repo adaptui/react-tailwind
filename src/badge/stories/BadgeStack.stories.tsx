9502892e300273af63cebca777961f69c76e047b
Navin Moorthy <navin007.a@gmail.com>
2021-10-18 15:45:16 +0530

    docs(badge): 📝 add badge stack sb (#180)

---
+++ src/badge/stories/BadgeStack.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+
+import { createControls, createPreviewTabs } from "../../../.storybook/utils";
+
+import js from "./templates/BadgeStackJsx";
+import ts from "./templates/BadgeStackTsx";
+import { BadgeStack } from "./BadgeStack.component";
+
+type Meta = ComponentMeta<typeof BadgeStack>;
+type Story = ComponentStoryObj<typeof BadgeStack>;
+
+export default {
+  title: "Primitives/Badge/Stack",
+  component: BadgeStack,
+  argTypes: createControls(undefined, {
+    ignore: [
+      "unstable_system",
+      "unstable_clickOnEnter",
+      "unstable_clickOnSpace",
+      "wrapElement",
+      "focusable",
+      "as",
+    ],
+  }),
+  parameters: {
+    layout: "centered",
+    options: { showPanel: true },
+    preview: createPreviewTabs({ js, ts }),
+  },
+} as Meta;
+
+export const Default: Story = {};
