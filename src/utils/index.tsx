d6b25efd655d54d49dc8d36cbb8bdd2a11b3c0c7
Navin Moorthy <navin007.a@gmail.com>
2021-10-18 18:14:58 +0530

    build(tw): 💚 revert to local tailwind-merge version

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { createTailwindMerge } from "tailwind-merge";
 
 import { cx } from "../utils";
 
+import { createTailwindMerge } from "./tailwind-merge";
 import {
   As,
   ComponentWithAs,
