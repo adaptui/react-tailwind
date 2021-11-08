48f18e09531b24b5d3452f6b259a65b7cdb3808d
Navin Moorthy <navin007.a@gmail.com>
2021-11-08 16:48:18 +0530

    refactor(preview): ♻️ remove react import

--- .storybook/utils.tsx
+++ .storybook/utils.tsx
@@ -1,4 +1,4 @@
-import react, * as React from "react";
+import * as React from "react";
 import type { Story } from "@storybook/react";
 import { CodeSandboxTemplate } from "storybook-addon-preview";
 
