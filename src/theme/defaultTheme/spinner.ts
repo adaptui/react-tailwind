7a06761ce3b58fefcf5a0003a298332ba4bcaa2c
Navin Moorthy <navin007.a@gmail.com>
2022-06-21 18:49:12 +0530

    refactor(spinner): ♻️ add current theme for track visible

--- src/theme/defaultTheme/spinner.ts
+++ src/theme/defaultTheme/spinner.ts
@@ -1,5 +1,5 @@
 export const spinner = {
-  base: "inline-block animate-spin",
+  base: "inline-block border-current animate-spin",
   size: {
     xs: "h-3 w-3 border-[1.5px] border-solid rounded-full",
     sm: "h-3.5 w-3.5 border-[1.5px] border-solid rounded-full",
@@ -22,6 +22,7 @@ export const spinner = {
       secondary: "border-b-violet-400 border-l-violet-400",
       success: "border-b-green-400 border-l-green-400",
       danger: "border-b-red-400 border-l-red-400",
+      current: "border-b-gray-400 border-l-gray-400",
     },
     transparent: "border-b-transparent border-l-transparent",
   },
