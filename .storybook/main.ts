d5c9ac2d1b939f2ecdafb2b2ed76097d9535ed5d
Navin Moorthy <navin007.a@gmail.com>
2022-06-30 13:16:15 +0530

    build(sb): 👷 update sb build to vite & remove babel build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,24 @@
+const { mergeConfig } = require("vite");
+
 const config = {
   framework: "@storybook/react",
-  core: { builder: "webpack5" },
+  core: { builder: "@storybook/builder-vite" },
+  async viteFinal(config: any, options: any) {
+    return mergeConfig(config, {
+      // customize the Vite config here
+      build: {
+        minify: false,
+        sourcemap: false,
+      },
+    });
+  },
+
   // storyStoreV7 removes the circular dependency issue with Webpack 5
   // So, we added ThemeProvider in preview.jsx and so src/theme should work for HMR
-  features: { storyStoreV7: true, babelModeV7: true },
+  // features: { storyStoreV7: true, babelModeV7: true },
   stories: ["../src/*/stories/*.stories.@(ts|tsx)"],
   addons: [
-    "storybook-addon-preview",
+    // "storybook-addon-preview",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
     {
